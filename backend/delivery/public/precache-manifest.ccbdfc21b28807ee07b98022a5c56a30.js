c2a246e6979d919a0527a6796fac57de5f5938af
Ivan Caceres <ivanny109@gmail.com>
2020-04-07 13:30:18 -0400

    production build

---
+++ backend/delivery/public/precache-manifest.ccbdfc21b28807ee07b98022a5c56a30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1db2704263797f23d6",
+    "url": "http://delivrbl.com/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "ac9dbcf41e9adf7c0319",
+    "url": "http://delivrbl.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc1db2704263797f23d6",
+    "url": "http://delivrbl.com/static/js/2.739355b1.chunk.js"
+  },
+  {
+    "revision": "f014ebc1cee314e3bc2c30a01c62c0ca",
+    "url": "http://delivrbl.com/static/js/2.739355b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9dbcf41e9adf7c0319",
+    "url": "http://delivrbl.com/static/js/main.0afe0377.chunk.js"
+  },
+  {
+    "revision": "d78f6b16c427facffdd3",
+    "url": "http://delivrbl.com/static/js/runtime-main.4868cb54.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "http://delivrbl.com/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "83d05c827e2e84026b6785695c562a75",
+    "url": "http://delivrbl.com/welcome.blade.php"
+  }
+]);
